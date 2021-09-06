7c948e692b0b87e7a34a73fb55f4d737a7a3e702
envitricollat <adria.salvador.p@gmail.com>
2021-09-06 20:58:04 +0200

    fix:fix pytest and add pytest configuration file

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -5,8 +5,8 @@ from corelib.translated_fortran_scripts import rho_real_python
 
 
 @pytest.mark.parametrize("pressure", [1, 6])
-@pytest.mark.unit
-def compare_benchmark_results(pressure):
+@pytest.mark.integration
+def test_benchmark_results(pressure):
     spherical_solver = BoltzmannSpherical(rho_correction=rho_real_python)
     spherical_solver.get_collision_integral()
     spherical_solver.solve_expansion(311, pressure)
