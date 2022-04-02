4e9b250f899be6d3ce5e043ee2640fd54e8519f6
envitricollat <adria.salvador.p@gmail.com>
2022-04-02 16:59:28 +0200

    adding span of sikora calculations

--- utils/intensity_calculator.py
+++ utils/intensity_calculator.py
@@ -2,6 +2,12 @@
 # in a microscope using an analytical intensity equation according
 # to certain parameters
 # pressure in pascals
+import itertools
+
+import numpy as np
+import pandas as pd
+
+from corelib.analytical_models.sikora import sikora_intensity
 from corelib.analytical_models.thermal_geometrical import TG_intensity
 
 pressure = 100e5
@@ -24,4 +30,14 @@ efficiency = 2.1e-6
 Intensity = TG_intensity(pressure, temperature, nozzle_radius, det_rad, xs, a)
 # multiply by detector efficiency
 Intensity = Intensity * efficiency
+# given a list of speed ratios and pressiures, get the sikora matrix
+S_r = [125, 300]
+T = 295
+P = np.arange(1, 100, 1)
+intensities = pd.DataFrame(index=P, columns=[str(element) for element in S_r])
+for el in itertools.product(S_r, P):
+    intensities.loc[el[1], str(el[0])] = sikora_intensity(
+        el[1], temperature, nozzle_radius, det_rad, xs, a, el[0], rs, xs
+    )
 print(Intensity)
+print(intensities)
